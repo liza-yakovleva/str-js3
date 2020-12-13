08f45ab6f14fe7d2a6bf05dd4378a4e09659ffa0
Elizaveta <lizayakovleva2005@gmail.com>
2020-12-13 19:42:33 +0200

    added fix

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // TODO Завдання.Написати функцію, яка буде перевіряти сайт на протокол HTTPS.Попросити користувача ввести посилання, і перевірити чи воно починається на протокол https, якщо так то вивести в консоль що сайт використовує https.
+
+
 let linkSite = prompt("Введите ссылку", "https://www.google.com/");
-linkSite.substring(0, 8) === "https://" ?
+function TestPath(link) {
+link.substring(0, 8) === "https://" ?
   console.log("Сайт використовує https://") :
   console.log("Сайт не використовує https://");
+  }
+  TestPath(linkSite);
